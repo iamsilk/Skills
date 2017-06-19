e619263f74a7ff0dc30d7aede2ea47f2297c8375
IAmSilK <iamsilk3495@gmail.com>
2017-06-19 13:05:40 -0300

    Created repository.

---
+++ CommandSkill.cs
@@ -0,0 +1,87 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Skills;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skills
+{
+    public class CommandSkill : IRocketCommand
+    {
+        public string Name => "skill";
+
+        public string Syntax => "[player] <skill> <level | max>";
+
+        public string Help => "Modify someone's skill sets.";
+
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+
+        public List<string> Permissions => new List<string>() { "skills.skill" };
+
+        public List<string> Aliases => new List<string>();
+
+        public void Execute(IRocketPlayer player, string[] parameters)
+        {
+            if (parameters.Length == 0)
+            {
+                UnturnedChat.Say(player, Skills.Instance.Translate("command_skill_help"));
+                return;
+            }
+
+            if (parameters.Length < 2 || parameters.Length > 3)
+            {
+                UnturnedChat.Say(player, Skills.Instance.Translate("invalid_parameters"), UnityEngine.Color.red);
+                return;
+            }
+            
+            UnturnedPlayer target;
+            int offset = 0;
+
+            if (parameters.Length == 2)
+            {
+                target = (UnturnedPlayer)player;
+            }
+            else
+            {
+                if (!player.HasPermission("skills.skill.other"))
+                {
+                    UnturnedChat.Say(player, Skills.Instance.Translate("no_permission"), UnityEngine.Color.red);
+                    return;
+                }
+
+                target = UnturnedPlayer.FromName(parameters[0]);
+                if (target == null)
+                {
+                    UnturnedChat.Say(player, Skills.Instance.Translate("player_not_found"), UnityEngine.Color.red);
+                    return;
+                }
+                offset++;
+            }
+
+            UnturnedSkill skill = Skills.GetSkill(parameters[0 + offset]);
+            if (skill == null)
+            {
+                UnturnedChat.Say(player, Skills.Instance.Translate("unknown_skill"), UnityEngine.Color.red);
+                return;
+            }
+
+            byte level;
+            if (parameters[1 + offset].ToLower() == "max")
+            {
+                level = Skills.GetMaxLevel(skill);
+            }
+            else if (!byte.TryParse(parameters[1 + offset], out level))
+            {
+                UnturnedChat.Say(player, Skills.Instance.Translate("invalid_level"), UnityEngine.Color.red);
+                return;
+            }
+
+            Skills.SetSkill(target, skill, level);
+            UnturnedChat.Say(player, Skills.Instance.Translate("skill_success"), UnityEngine.Color.red);
+        }
+    }
+}
---
+++ Skills.cs
@@ -0,0 +1,130 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Skills;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API.Collections;
+
+namespace Skills
+{
+    public class Skills : RocketPlugin
+    {
+        public static Skills Instance;
+
+        protected override void Load()
+        {
+            Instance = this;
+        }
+
+        public static UnturnedSkill GetSkill(string skill)
+        {
+            switch (skill.ToLower())
+            {
+                case "overkill": return UnturnedSkill.Overkill;
+                case "sharpshooter": return UnturnedSkill.Sharpshooter;
+                case "dexterity": return UnturnedSkill.Dexerity;
+                case "cardio": return UnturnedSkill.Cardio;
+                case "exercise": return UnturnedSkill.Exercise;
+                case "diving": return UnturnedSkill.Diving;
+                case "parkour": return UnturnedSkill.Parkour;
+                case "sneakybeaky": return UnturnedSkill.Sneakybeaky;
+                case "vitality": return UnturnedSkill.Vitality;
+                case "immunity": return UnturnedSkill.Immunity;
+                case "toughness": return UnturnedSkill.Toughness;
+                case "strength": return UnturnedSkill.Strength;
+                case "warmblooded": return UnturnedSkill.Warmblooded;
+                case "survival": return UnturnedSkill.Survival;
+                case "healing": return UnturnedSkill.Healing;
+                case "crafting": return UnturnedSkill.Crafting;
+                case "outdoors": return UnturnedSkill.Outdoors;
+                case "cooking": return UnturnedSkill.Cooking;
+                case "fishing": return UnturnedSkill.Fishing;
+                case "agriculture": return UnturnedSkill.Agriculture;
+                case "mechanic": return UnturnedSkill.Mechanic;
+                case "engineer": return UnturnedSkill.Engineer;
+            }
+            return null;
+        }
+
+        public static byte GetMaxLevel(UnturnedSkill skill)
+        {
+            if (skill == UnturnedSkill.Overkill) return 7;
+            if (skill == UnturnedSkill.Sharpshooter) return 7;
+            if (skill == UnturnedSkill.Dexerity) return 5;
+            if (skill == UnturnedSkill.Cardio) return 5;
+            if (skill == UnturnedSkill.Exercise) return 5;
+            if (skill == UnturnedSkill.Diving) return 5;
+            if (skill == UnturnedSkill.Parkour) return 5;
+            if (skill == UnturnedSkill.Sneakybeaky) return 7;
+            if (skill == UnturnedSkill.Vitality) return 5;
+            if (skill == UnturnedSkill.Immunity) return 5;
+            if (skill == UnturnedSkill.Toughness) return 5;
+            if (skill == UnturnedSkill.Strength) return 5;
+            if (skill == UnturnedSkill.Warmblooded) return 5;
+            if (skill == UnturnedSkill.Survival) return 5;
+            if (skill == UnturnedSkill.Healing) return 7;
+            if (skill == UnturnedSkill.Crafting) return 3;
+            if (skill == UnturnedSkill.Outdoors) return 5;
+            if (skill == UnturnedSkill.Cooking) return 3;
+            if (skill == UnturnedSkill.Fishing) return 5;
+            if (skill == UnturnedSkill.Agriculture) return 7;
+            if (skill == UnturnedSkill.Mechanic) return 5;
+            if (skill == UnturnedSkill.Engineer) return 3;
+            return 0;
+        }
+
+        public static void GiveMaxSkills(UnturnedPlayer player)
+        {
+            SetSkill(player, UnturnedSkill.Overkill, 7);
+            SetSkill(player, UnturnedSkill.Sharpshooter, 7);
+            SetSkill(player, UnturnedSkill.Dexerity, 5);
+            SetSkill(player, UnturnedSkill.Cardio, 5);
+            SetSkill(player, UnturnedSkill.Exercise, 5);
+            SetSkill(player, UnturnedSkill.Diving, 5);
+            SetSkill(player, UnturnedSkill.Parkour, 5);
+            
+            SetSkill(player, UnturnedSkill.Sneakybeaky, 7);
+            SetSkill(player, UnturnedSkill.Vitality, 5);
+            SetSkill(player, UnturnedSkill.Immunity, 5);
+            SetSkill(player, UnturnedSkill.Toughness, 5);
+            SetSkill(player, UnturnedSkill.Strength, 5);
+            SetSkill(player, UnturnedSkill.Warmblooded, 5);
+            SetSkill(player, UnturnedSkill.Survival, 5);
+            
+            SetSkill(player, UnturnedSkill.Healing, 7);
+            SetSkill(player, UnturnedSkill.Crafting, 3);
+            SetSkill(player, UnturnedSkill.Outdoors, 5);
+            SetSkill(player, UnturnedSkill.Cooking, 3);
+            SetSkill(player, UnturnedSkill.Fishing, 5);
+            SetSkill(player, UnturnedSkill.Agriculture, 7);
+            SetSkill(player, UnturnedSkill.Mechanic, 5);
+            SetSkill(player, UnturnedSkill.Engineer, 3);
+        }
+
+        public static void SetSkill(UnturnedPlayer player, UnturnedSkill skill, byte level)
+        {
+            player.SetSkillLevel(skill, level);
+        }
+
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            { "command_skill_help", "/skill [player] <skill> <level | max> - Modify your own or someone else's skill." },
+            { "command_maxskills_help", "/maxskills [player] - Give yourself or someone max skills." },
+            { "command_zeroskills_help", "/zeroskills [player] - Set your own or someone else's skills to zero." },
+
+            { "skill_success", "Successfully modified the skill." },
+            { "maxskills_success", "Successfully maxed out skills." },
+            { "zeroskills_success", "Successfully zeroed skills." },
+
+            { "unknown_skill", "The specified skill does not exist." },
+            { "invalid_level", "The specified level is invalid." },
+
+            { "player_not_found", "Cannot find the specified player." },
+            { "no_permission", "You do not have permission to do this." },
+            { "invalid_parameters", "Invalid parameters." },
+        };
+    }
+}
